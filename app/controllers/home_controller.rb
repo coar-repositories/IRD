876debcf9a3e87809479a5c9ecae22f799ef23c0
paulwalk <paul@paulwalk.net>
2025-03-16 14:57:31 +0000

    improved home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
   def index
     @page_title = t('page_titles.welcome')
-    @stats = Stats::SystemSetStatsService.call(System.publicly_viewable, "All Repositories").payload
+    @stats = Stats::SystemSetStatsService.call(System.all, "All Repositories").payload
     continent_data = []
     Country.translated_continents.each do |translated_continent|
       # unless ['global', 'antarctica'].include? translated_continent[1]
-      systems = System.joins(:country).where(countries: { continent: translated_continent[2] }).publicly_viewable
+      systems = System.joins(:country).where(countries: { continent: translated_continent[2] })
       continent_data << ["#{translated_continent[0]} (#{systems.count})", systems.count]
       # end
     end
