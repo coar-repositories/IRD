0e97b34c7f90d41d7bc9b6f272234f20c9834b0a
paulwalk <paul@paulwalk.net>
2025-03-24 14:01:54 +0000

    fixed OAI check

--- app/services/oai_pmh/oai_pmh_identify_service.rb
+++ app/services/oai_pmh/oai_pmh_identify_service.rb
@@ -26,7 +26,7 @@ module OaiPmh
           else
             @system.oai_status = :unsupported
           end
-          return system # return early if no OAI-PMH base URL
+          raise StandardError.new("OAI-PMH Base URL not set")
         end
         original_url_with_verbs_removed = Utilities::OaiPmhUrlFormatter.without_verbs(original_url)
         url_with_verb_identify = Utilities::OaiPmhUrlFormatter.with_verb_identify(original_url)
@@ -55,35 +55,43 @@ module OaiPmh
         else
           @system.oai_status = :not_enabled
         end
+        failure e
       rescue Faraday::ForbiddenError => e # 403
         Rails.logger.warn("#{e} for OAI-PMH Identify #{url_with_verb_identify}")
         @system.write_network_check(:oai_pmh_identify, false, e.message, e.response[:status])
         @system.oai_status = :offline
+        failure e
       rescue Faraday::FollowRedirects::RedirectLimitReached => e
         Rails.logger.warn("#{e} for OAI-PMH Identify #{url_with_verb_identify}")
         @system.write_network_check(:oai_pmh_identify, false, e.message, 0)
         @system.oai_base_url = nil
         @system.oai_status = :offline
+        failure e
       rescue Faraday::ClientError => e # 4xx
         Rails.logger.warn("#{e} for OAI-PMH Identify #{url_with_verb_identify}")
         @system.write_network_check(:oai_pmh_identify, false, e.message, e.response[:status])
         @system.oai_status = :offline
+        failure e
       rescue Faraday::TimeoutError => e
         Rails.logger.warn("#{e} for OAI-PMH Identify #{url_with_verb_identify}")
         @system.write_network_check(:oai_pmh_identify, false, e.message, 0)
         @system.oai_status = :offline
+        failure e
       rescue Faraday::NilStatusError => e
         Rails.logger.warn("#{e} for OAI-PMH Identify #{url_with_verb_identify}")
         @system.write_network_check(:oai_pmh_identify, false, e.message, 0)
         @system.oai_status = :offline
+        failure e
       rescue Faraday::ServerError => e # 5xx
         Rails.logger.warn("#{e} for OAI-PMH Identify #{url_with_verb_identify}")
         @system.write_network_check(:oai_pmh_identify, false, e.message, e.response[:status])
         @system.oai_status = :offline
+        failure e
       rescue Faraday::SSLError => e
         Rails.logger.warn("#{e} for OAI-PMH Identify #{url_with_verb_identify}")
         @system.write_network_check(:oai_pmh_identify, false, e.message, 0)
         @system.oai_status = :offline
+        failure e
       rescue Faraday::ConnectionFailed => e
         Rails.logger.warn("#{e} for OAI-PMH Identify #{url_with_verb_identify}")
         @system.write_network_check(:oai_pmh_identify, false, e.message, 0)
@@ -94,10 +102,12 @@ module OaiPmh
         # else
         #   @system.oai_status = :not_enabled
         # end
+        failure e
       rescue Faraday::Error, StandardError => e
         Rails.logger.warn("#{e} for OAI-PMH Identify #{url_with_verb_identify}")
         @system.write_network_check(:oai_pmh_identify, false, e.message, 0)
         @system.oai_status = :unknown
+        failure e
       end
       success @system
     end
